741dacc7c3259559a6c38765afaf30569e63fe74
ruxne <11211005@student.itk.ac.id>
2022-12-30 18:21:12 +0800

    add eslint

--- src/server.js
+++ src/server.js
@@ -2,20 +2,20 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-	const server = Hapi.server({
-		port: 5000,
-		host: 'localhost',
-		// CORS
-		routes: {
-			cors: {
-				origin: ['*'],
-			},
-		},
-	});
-	await server.start();
-	console.log(`Server berjalan pada ${server.info.uri}`);
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    // CORS
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 
-	server.route(routes);
+  server.route(routes);
 };
 
 
